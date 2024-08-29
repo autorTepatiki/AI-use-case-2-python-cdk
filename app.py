8e30ca79884645791052b971a0806e90f9c092ee
Martin D Cernadas <eb_mcernadas@bancoripley.com>
2024-08-28 21:04:20 -0300

    updated-app

--- app.py
+++ app.py
@@ -3,17 +3,10 @@ import os
 
 import aws_cdk as cdk
 
-from ai_use_case_2_python_cdk.ai_use_case_2_python_cdk_stack import AiUseCase2PythonCdkStack
-
 from huggingface_sagemaker.huggingface_stack import HuggingfaceSagemaker
 
 
 app = cdk.App()
-
-# https://docs.aws.amazon.com/cdk/latest/guide/environments.htm
-AiUseCase2PythonCdkStack(app, "AiUseCase2PythonCdkStack", 
-    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-    )
 
 sagemaker = HuggingfaceSagemaker(app, "HuggingfaceServerlessSagemakerEndpoint", 
     env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
